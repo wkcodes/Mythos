6722aba1a3c512d79cf7a675b6774360669e0fed
Will Kendall <will@wills-MacBook-Pro.local>
2020-11-21 13:09:29 -0800

    modified migration file

--- migrations/ImgMigration.js
+++ migrations/ImgMigration.js
@@ -1,21 +1,14 @@
-export function up(queryInterface, Sequelize) {
-    // logic for transforming into the new state
-    return queryInterface.addColumn(
-        'users',
-        'img1',
-        Sequelize.STRING
-    )
-}
-
-export function up(queryInterface, Sequelize) {
-    // logic for transforming into the new state
-    return queryInterface.addColumn(
-        'users',
-        'img2',
-        Sequelize.STRING
-    )
-}
-
-export function down(queryInterface, Sequelize) {
-    // logic for reverting the changes
-}
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+      // logic for transforming into the new state
+      return queryInterface.addColumn(
+          'users',
+          'img1',
+          Sequelize.STRING
+      )
+    },
+   
+    down: function(queryInterface, Sequelize) {
+      // logic for reverting the changes
+    }
+  }
