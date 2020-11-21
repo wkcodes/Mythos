7374674d3bd6310341361b89ab0a784ac8c11fc4
DuRyan Ebbesen <duryanebbesen@yahoo.com>
2020-11-21 13:34:19 -0800

    rmv player-card and added a new folder, mythosPlayerCard

---
+++ mythosPlayerCard/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron/index';
+
+const Home = () => {
+    return (
+    <div>
+        <Jumbotron></Jumbotron>
+        
+    </div>
+  );
+}
+
+export default Home;
