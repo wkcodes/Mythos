b522d3d3725597e535d906f7351e36751e0c4de7
Josh Payne <paynejoshua1@gmail.com>
2020-11-23 20:08:32 -0800

    fixed folder structure so that React now opens automatically

---
+++ client/src/pages/Profile.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Jumbotron from '../components/Jumbotron/index';
+
+const Profile = () => {
+    return (
+    <div>
+        <Jumbotron></Jumbotron>
+        
+    </div>
+  );
+}
+
+export default Profile;
