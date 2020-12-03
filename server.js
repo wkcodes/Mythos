bccfdd2c845367cd77c403871baedcdbd16d38b8
Will Kendall <will@wills-MacBook-Pro.local>
2020-12-03 13:36:14 -0800

    added / in server

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ require("./routes/profile-api-routes.js")(app);
 require("./routes/login-api-routes.js")(app);
 require("./routes/signupRoute.js")(app)
 
-app.get("*", function(req, res) {
+app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 // Syncing our sequelize models and then starting our Express app
