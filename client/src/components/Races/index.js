ac918859241dc7717de3b1cf5a69828e049c9251
Josh Payne <paynejoshua1@gmail.com>
2020-12-05 16:22:38 -0800

    path should now be fixed

--- client/src/components/Races/index.js
+++ client/src/components/Races/index.js
@@ -4,55 +4,55 @@ const Race = {
 
     elf: {
         name: "Elf",
-        img1: "build/images/templates/Races/Elf/Base-Elf.png",
+        img1: "../../images/templates/Races/Elf/Base-Elf.png",
         weapons: [
             {
                 name: "sword",
-                img: "build/images/templates/Races/Elf/Elf-Sword.png"
+                img: "../../images/templates/Races/Elf/Elf-Sword.png"
             },
             {
                 name: "axe",
-                img: "../images/templates/Races/Elf/Elf-Axe.png"
+                img: "../../images/templates/Races/Elf/Elf-Axe.png"
             },
             {
                 name: "bow",
-                img: "../images/templates/Races/Elf/Elf-Bow-and-Quiver.png"
+                img: "../../images/templates/Races/Elf/Elf-Bow-and-Quiver.png"
             },
         ]
     },
     human: {
         name: "Human",
-        img1: "../images/templates/Races/Human/Base-Human.png",
+        img1: "../../images/templates/Races/Human/Base-Human.png",
         weapons: [
             {
                 name: "Sword",
-                img: "../images/templates/Races/Human/Human-Sword.png"
+                img: "../../images/templates/Races/Human/Human-Sword.png"
             },
             {
                 name: "Axe",
-                img: "../images/templates/Races/Human/Human-Axe.png"
+                img: "../../images/templates/Races/Human/Human-Axe.png"
             },
             {
                 name: "Bow",
-                img: "../images/templates/Races/Human/Human-Bow-and-Quiver.png"
+                img: "../../images/templates/Races/Human/Human-Bow-and-Quiver.png"
             }
         ]
     },
     dwarf: {
         name: "Dwarf",
-        img1: "../images/templates/Races/Dwarf/Base-Dwarf.png",
+        img1: "../../images/templates/Races/Dwarf/Base-Dwarf.png",
         weapons: [
             {
                 name: "Sword",
-                img: "../images/templates/Races/Dwarf/Dwarf-Sword.png"
+                img: "../../images/templates/Races/Dwarf/Dwarf-Sword.png"
             },
             {
                 name: "Axe",
-                img: "../images/templates/Races/Dwarf/Dwarf-Axe.png"
+                img: "../../images/templates/Races/Dwarf/Dwarf-Axe.png"
             },
             {
                 name: "Bow",
-                img: "../images/templates/Races/Dwarf/Dwarf-Bow-and-Quiver.png"
+                img: "../../images/templates/Races/Dwarf/Dwarf-Bow-and-Quiver.png"
             }
         ]
     }
